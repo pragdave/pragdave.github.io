3ea2f6530b68e9b9fc2e2337d17022223c24e58e
pragdave <dave@pragdave.me>
2017-01-19 20:41:43 -0600

    lazyload media

--- _assets/javascripts/application/speak.js
+++ _assets/javascripts/application/speak.js
@@ -2,20 +2,32 @@
 // to the top
 
 
+function move_hashed_content_to_top() {
+    var hash = window.location.hash;
+    if (hash) {
+        var $small_element = $(hash);
+        if ($small_element) {
+            $small_element.hide();
+            var $showcase = $(".showcase");
+            var $holder = $showcase.find("#holder");
+            $holder.html($small_element.html());
+            $holder.addClass($small_element.attr("class"));
+            $showcase.show(500);
+            $(window).scrollTop(0);
+        }
+    }
+}
+
 $(function () {
     if ($(".speak")) {
-        var hash = window.location.hash;
-        if (hash) {
-            var $small_element = $(hash);
-            if ($small_element) {
-                $small_element.hide();
-                var $showcase = $(".showcase");
-                var $holder = $showcase.find("#holder");
-                $holder.html($small_element.html());
-                $holder.addClass($small_element.attr("class"));
-                $showcase.show(500);
-                $(window).scrollTop(0);
-            }
-        }
+        move_hashed_content_to_top();
+
+        $(".card-columns").on("click", "figcaption", function(ev) {
+            ev.preventDefault();
+            let new_hash = this.parentNode.id;
+            window.location.hash = new_hash;
+            $("figure").show(200);
+            move_hashed_content_to_top();
+        });
     }
 });
--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -2,4 +2,10 @@
 //= require tether/dist/js/tether
 //= require bootstrap/dist/js/bootstrap
 //= require urijs/src/URI
+//= require lazyloadxt/dist/jquery.lazyloadxt.extra
 
+//= require_self
+
+$.extend($.lazyLoadXT, {
+    blankImage: "/other_images/gnome-tapping-foot.gif"
+});
