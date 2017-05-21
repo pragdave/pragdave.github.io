d1860684338b35a318f2efa9538632452e3bb2f4
pragdave <dave@pragdave.me>
2017-05-21 14:11:08 -0500

    add headshots

--- _plugins/liquid-tags.rb
+++ _plugins/liquid-tags.rb
@@ -166,7 +166,7 @@ module LiquidTags
     end
 
     def headshot_link(format)
-      url = format[1].sub(/.*images/, "/assets")
+      url = format[1].sub(/.*images/, "/headshots")
       size = format[0]
       %{<a href="#{url}">#{size}</a>}
     end
